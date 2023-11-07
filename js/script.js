5818f39c13f162c733344ab7c0a46cdf62717d46
koutamanto <58100773+koutamanto@users.noreply.github.com>
2023-11-07 10:21:52 +0900

    ウェ〜イ

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 window.onload = function() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '/products.json');
-    xhr.onloadend = (event) => {
+    xhr.onload = (event) => {
         var productInfos = JSON.parse(xhr.responseText);
-        for(var i=0;i++;i<productInfos.list.length) {
-            document.getElementsByClassName("product-image")[i].setAttribute("src", productInfos.list[i].image);
+        console.log(productInfos)
+        for(var i=0;i<productInfos.list.length;i++) {
+            console.log(productInfos.list[i].image);
+            var linkTo = productInfos.list[i].link;
+            document.getElementsByClassName("product-image")[i].src = productInfos.list[i].image;
+            document.getElementsByClassName("buy-btn")[i].addEventListener("click", function () {
+                location = linkTo;
+            });
         }
     };
     xhr.send();
